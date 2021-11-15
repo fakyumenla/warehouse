96386d9db8ba62ab068983b96f68f9ee42651c58
fakyumenla <gunturburn@gmail.com>
2021-11-15 16:31:52 +0700

    guarded

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -14,7 +14,7 @@ class ItemsController extends Controller
      */
     public function index()
     {
-        return view('pages.admin.Employee.index');
+        return view('pages.admin.Item.index');
     }
 
     /**
--- app/Models/Transaction_owner.php
+++ app/Models/Transaction_owner.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction_owner extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
 }
