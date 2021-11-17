e2ca190c8de0fed2ed8c6387fd79b6ead5cdbfe3
fakyumenla <gunturburn@gmail.com>
2021-11-17 17:43:44 +0700

    read the 2nd

--- app/Http/Controllers/EmployeesController.php
+++ app/Http/Controllers/EmployeesController.php
@@ -16,6 +16,17 @@ class EmployeesController extends Controller
      */
     public function index()
     {
+        if (request()->ajax()) {
+            $data = Employee::latest()->get();
+            return Datatables::of($data)
+                ->addIndexColumn()
+                ->addColumn('action', function($row){
+                    $actionBtn = '<a href="javascript:void(0)" class="edit btn btn-success btn-sm">Edit</a> <a href="javascript:void(0)" class="delete btn btn-danger btn-sm">Delete</a>';
+                    return $actionBtn;
+                })
+                ->rawColumns(['action'])
+                ->make(true);
+        }
         return view('pages.admin.Employee.index');
     }
 
